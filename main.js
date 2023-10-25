066a2a7016bd89cef48f04b5adbf78b7b47b64bd
dojeinthemoon <117288488+dojeinthemoon@users.noreply.github.com>
2023-10-25 11:43:06 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+const http =  require('http');
+const fs = require('fs');
+
+
+fs.readfile('','utf8',(err,data)=>{
+    console.log(data);
+});
+
+app.get('/', (req,res)=>{
+    data
+})
+server.listen(3000,()=>{
+    console.log('server running on PORT 3000');
+});
+
+fs.readfile('','utf8',(err,data)=>{
+    console.log(data);
+});
